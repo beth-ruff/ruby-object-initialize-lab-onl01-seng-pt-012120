18578233730f94ebb5c5065a1112f8fed568003c
Beth Ruff <beth.ruff2@gmail.com>
2020-02-07 01:01:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,19 +5,6 @@ class Dog
     @breed = breed
   end
   
-  def name=(name, breed)
-    @name = name 
-    @breed = breed
-  end
-  
-  def name
-    @name 
-  end
-  
-  def breed 
-    @breed 
-  end
-
 end
 
 fido = Dog.new("Fido")
